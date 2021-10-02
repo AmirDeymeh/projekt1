efd91a00a915210b17a7bffd339aa3772f4e8f04
alideymeh <alideymeh9@gmail.com>
2021-10-02 20:47:56 +0330

    first commit

---
+++ jsudemy.js
@@ -0,0 +1,13 @@
+const menuSab=document.getElementById('menu-sab');
+const sabMenu=document.querySelector('.sab-menu');
+menuSab.addEventListener('click',function(){
+    sabMenu.classList.toggle('activ')
+})
+
+
+
+const buttonmd=document.getElementById('buttonmd');
+const navmd=document.querySelector('.navmd');
+buttonmd.addEventListener('click',function(){
+    navmd.classList.toggle('activ')
+})
